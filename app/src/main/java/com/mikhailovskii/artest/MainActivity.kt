3f36d78d63829c1cc90d545b80dc8778e3b39c89
Sergei Mikhailovskii <mikhailovskii.s@tut.by>
2020-03-03 16:21:45 +0300

    Initial commit

---
+++ app/src/main/java/com/mikhailovskii/artest/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.mikhailovskii.artest
+
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.google.ar.core.Anchor
+import com.google.ar.sceneform.AnchorNode
+import com.google.ar.sceneform.rendering.ModelRenderable
+import com.google.ar.sceneform.ux.ArFragment
+import com.google.ar.sceneform.ux.TransformableNode
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var arFragment: ArFragment
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        arFragment = supportFragmentManager.findFragmentById(R.id.arFragment) as ArFragment
+
+        arFragment.setOnTapArPlaneListener { hitResult, _, _ ->
+            val anchor = hitResult.createAnchor()
+
+            ModelRenderable.builder()
+                .setSource(this, Uri.parse("duck.sfb"))
+                .build()
+                .thenAccept { modelRenderable -> addModelToScene(anchor, modelRenderable) }
+                .exceptionally { throwable ->
+                    Toast.makeText(this, throwable.message, Toast.LENGTH_SHORT).show()
+                    null
+                }
+        }
+
+    }
+
+    private fun addModelToScene(anchor: Anchor, modelRenderable: ModelRenderable) {
+        val anchorNode = AnchorNode(anchor)
+        val transformableNode = TransformableNode(arFragment.transformationSystem)
+        transformableNode.setParent(anchorNode)
+        transformableNode.renderable = modelRenderable
+        arFragment.arSceneView.scene.addChild(anchorNode)
+        transformableNode.select()
+    }
+
+}
